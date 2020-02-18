9b29d23077f29127fc41614fbf244f23fe7d2f81
felix <felixfotball99@gmail.com>
2020-02-18 14:55:21 +0100

    Endret litt på design i scenebuilder

--- src/src/DateCheck.java
+++ src/src/DateCheck.java
@@ -1,7 +1,7 @@
 public class DateCheck {
 
     String dato;
-    String dateRegex = "^([0-2][0-9]||3[0-1])/(0[0-9]||1[0-2])/([0-9][0-9])?[0-9][0-9]$";
+    String dateRegex = "[0-9]{2}[/]{1}[0-9]{2}[/]{1}[0-9]{4}";
 
 
 
